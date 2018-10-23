408cd28e57925f7028d85b6137719bdcdd62f238
halimwi <hwijaya@gmail.com>
2018-10-23 17:03:50 +0800

    add download and install switch

--- tasks/win_update.rb
+++ tasks/win_update.rb
@@ -61,11 +61,11 @@ begin
   if stdout
     case stdout.strip
     when '3da21691-e39d-4da6-8a4b-b43877bcb1b7'
-      cmd_string = "powershell -command \"Import-Module PSWindowsUpdate; Get-WUInstall -AcceptAll #{allow_reboot}\""
+      cmd_string = "powershell -command \"Import-Module PSWindowsUpdate; Get-WUInstall -verbose -IgnoreUserInput -Download -Install -AcceptAll #{allow_reboot}\""
     when '9482f4b4-e343-43b6-b170-9a65bc822c77'
-      cmd_string = "powershell -command \"Import-Module PSWindowsUpdate; Get-WUInstall -WindowsUpdate -AcceptAll #{allow_reboot}\""
+      cmd_string = "powershell -command \"Import-Module PSWindowsUpdate; Get-WUInstall -verbose -IgnoreUserInput -Download -Install -WindowsUpdate -AcceptAll #{allow_reboot}\""
     when '7971f918-a847-4430-9279-4a52d1efe18d'
-      cmd_string = "powershell -command \"Import-Module PSWindowsUpdate; Get-WUInstall -MicrosoftUpdate -AcceptAll #{allow_reboot}\""
+      cmd_string = "powershell -command \"Import-Module PSWindowsUpdate; Get-WUInstall -verbose -IgnoreUserInput -Download -Install -MicrosoftUpdate -AcceptAll #{allow_reboot}\""
     else
       puts 'No Update Services configured'
       exit 0
