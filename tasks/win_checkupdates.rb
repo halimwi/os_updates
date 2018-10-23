aff7d718bba99f4d86da62b90dad92158819a3ec
halimwi <hwijaya@gmail.com>
2018-10-23 15:10:16 +0800

    allow non-manage Windows update

--- tasks/win_checkupdates.rb
+++ tasks/win_checkupdates.rb
@@ -49,7 +49,7 @@ end
 
 begin
   # Find if we are using WSUS or Windows Update
-  manager_cmd = "powershell -command \"Import-Module PSWindowsUpdate; Get-WUServiceManager | Where-Object {$_.IsManaged -eq 'true'} | foreach {$_.ServiceID}\""
+  manager_cmd = "powershell -command \"Import-Module PSWindowsUpdate; Get-WUServiceManager | foreach {$_.ServiceID}\""
   stdout, stderr, status = Open3.capture3(manager_cmd)
   raise 'Cannot get Windows Update configurations', stderr if status != 0
   # Determine which service is enable can use that to check for patches and updates
